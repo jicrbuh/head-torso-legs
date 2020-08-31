23993f8cab9ef1b5ce0f41ef312dc42c52697ebf
jicrbuh <jicrbuh1@gmail.com>
2020-08-31 21:04:59 +0300

    first commit

---
+++ app/src/main/java/com/example/connectit/NewGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.connectit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_game);
+    }
+}
