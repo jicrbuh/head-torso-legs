bafe5c07b5541262412c065cc6d9a7becea90559
jicrbuh <jicrbuh1@gmail.com>
2020-10-07 14:07:28 +0300

    save image in storage under body part name

--- app/src/main/java/com/example/HeadTorsoLegs/DrawActivity.java
+++ app/src/main/java/com/example/HeadTorsoLegs/DrawActivity.java
@@ -1,89 +1,60 @@
 package com.example.HeadTorsoLegs;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.*;
 import com.google.firebase.storage.*;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.res.ResourcesCompat;
+
 import android.widget.Toast;
 import com.example.headtorsolegs.R;
+import com.google.gson.Gson;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.util.UUID;
+
 
 public class DrawActivity extends Activity {
     //https://google-developer-training.github.io/android-developer-advanced-course-practicals/unit-5-advanced-graphics-and-views/lesson-11-canvas/11-1a-p-create-a-simple-canvas/11-1a-p-create-a-simple-canvas.html
     Button buttonFinished;
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReference("test/");
-    StorageReference mountainsRef = storageRef.child("mountains.jpg");
+    StorageReference drawingRef;
 
-    public String getURLForResource (int resourceId) {
-        //use BuildConfig.APPLICATION_ID instead of R.class.getPackage().getName() if both are not same
-        return Uri.parse("android.resource://"+R.class.getPackage().getName()+"/" +resourceId).toString();
+
+    private void createDrawingRef() {
+        Gson gson = new Gson();
+        SharedPreferences sharedpreferences = getSharedPreferences(MyConstants.SharedPREFERENCE, Context.MODE_PRIVATE);
+        String userDataJson = sharedpreferences.getString(MyConstants.UserDataKEY, "");
+        UserData userData = gson.fromJson(userDataJson, UserData.class);
+        drawingRef = storageRef.child("drawing_" + userData.getDrawingPosition() + ".jpg");
     }
 
-
-    public void firebaseExample() {
-        //final String path = "drawable/flower.bmp";
-        final String imageUri = "drawable://" + R.drawable.flower;
-        final String path = getURLForResource(R.drawable.flower);
-        Uri file = Uri.fromFile(new File(path));
-
-        StorageReference riversRef = storageRef.child("test/"+"hello");
-        UploadTask uploadTask = riversRef.putFile(file);
-
-        // Register observers to listen for when the download is done or if it fails
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful uploads
-                Toast.makeText(getApplicationContext(), "Failed Upload: " + path, Toast.LENGTH_LONG).show();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-
-            }
-        });
-    }
-
-    private void uploadView(DrawView drawview) {
+    private void uploadDrawView(DrawView drawView) {
 
         // Get the data from an ImageView as bytes
-        drawview.setDrawingCacheEnabled(true);
-        drawview.buildDrawingCache();
-        Bitmap bitmap = drawview.getCanvasBitmap();
+        drawView.setDrawingCacheEnabled(true);
+        drawView.buildDrawingCache();
+        Bitmap bitmap = drawView.getCanvasBitmap();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] data = baos.toByteArray();
 
-        UploadTask uploadTask = mountainsRef.putBytes(data);
+        UploadTask uploadTask = drawingRef.putBytes(data);
         uploadTask.addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
                 // Handle unsuccessful uploads
+                Toast.makeText(getApplicationContext(), "Failed to Upload: please try again", Toast.LENGTH_LONG).show();
+
             }
         }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
@@ -94,22 +65,22 @@ public class DrawActivity extends Activity {
         });
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.draw_screen);
         buttonFinished = (Button)findViewById(R.id.buttonFinished);
-        Log.i("chello!", "before firebase");
-        //firebaseExample();
 
-        // go to after drawing room
         buttonFinished.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final DrawView drawing = findViewById(R.id.myimageview);
-                uploadView(drawing);
+                createDrawingRef();
+                uploadDrawView(drawing);
+
+                // go to after drawing room
                 Intent intent = new Intent(view.getContext(), AfterDrawingActivity.class);
                 view.getContext().startActivity(intent);
             }
